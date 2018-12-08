f01627ccdd8c5e0068000698b53346d43b9589d2
MaynardMiner <maynardviii@gmail.com>
2018-12-07 21:02:31 -0500

    v1.0

--- VII-smi.c
+++ VII-smi.c
@@ -18,8 +18,10 @@ int main()
         nvmlDevice_t device;
         nvmlReturn_t result1;
         nvmlReturn_t result2;
-        unsigned int fanspeed;
-        unsigned int temperature;
+        nvmlReturn_t result3;
+        unsigned int fanspeed=0;
+        unsigned int temperature=0;
+        unsigned int power=0;
         nvmlComputeMode_t sensor = 0;
 
        result = nvmlDeviceGetHandleByIndex(i, &device);
@@ -27,16 +29,22 @@ int main()
        { 
         result1 = nvmlDeviceGetFanSpeed(device, &fanspeed);
         result2 = nvmlDeviceGetTemperature ( device, sensor, &temperature );
+        result3 = nvmlDeviceGetPowerUsage ( device, &power );
          if(NVML_SUCCESS == result1)
          { 
-          printf("%i fan speed=%d\n", i, fanspeed);
+          printf("GPU %i fan speed=%d\n", i, fanspeed);
          }
-         else printf("Failed to get fan speed for device %i:\n", i);
+         else printf("GPU %i fan speed=failed to get\n", i);
          if (NVML_SUCCESS == result2)
          {
-          printf("%i temperature=%d\n", i, temperature);
+          printf("GPU %i temperature=%d\n", i, temperature);
          }
-         else printf("Failed to get temperature for device %i\n", i);
+         else printf("GPU %i temperature=failed to get\n", i);
+         if(NVML_SUCCESS == result3)
+         {
+         printf("GPU %i power=%d\n", i, power/1000);
+         }
+         else printf("GPU %i power=failed to get\n", i);
         }
        else printf("Failed to get handle for device %i\n", i);
      }
